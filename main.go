d548a48fbe5604576af86108dbcff80483e98abb
Modlizhka <fierce_type@tutamail.com>
2024-12-12 00:33:22 +0700

    release

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+var justString string
+
+// Длина строки может быть меньше 100 произойдет паника, нужно добавить проверку длины строки
+func someFunc() {
+	v := createHugeString(1 << 10)
+	if len(v) >= 100 {
+		justString = v[:100]
+	}
+
+}
+
+func main() {
+	someFunc()
+}
